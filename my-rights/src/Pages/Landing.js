7cc8cc6d5fe8a57e9aacc8c4ea4761122a22e38c
rachel <rachellin443@gmail.com>
2023-01-21 16:32:52 +0000

    css changes to buttons

--- my-rights/src/Pages/Landing.js
+++ my-rights/src/Pages/Landing.js
@@ -1,11 +1,24 @@
 
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../Css/Landing.css';
 
 const Landing=()=> {
 return(
     <>
-    <div className="App">
-        <h1>My Rights</h1>
+    <div className='container'>
+        <div className='contentBox'>
+            <h1>Human Rights Project</h1>
+            <p>blah blah</p>
+            <div className='btnBox'>
+                <div className='btn'>
+                    <NavLink to="/profile" className="policeProfile">Register as Police Officer</NavLink>
+                </div>
+                <div className='btn'>
+                    <NavLink to="/search" className="searchProfile">Find a Police Officer</NavLink>
+                </div>
+            </div>
+        </div>
     </div>
     </>
 )
