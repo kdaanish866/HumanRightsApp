b250dcc9f2125472719754501eb35c8bd45e38b5
sramesh <62487541+sahanarameshh@users.noreply.github.com>
2023-01-21 20:11:07 +0000

    profile stats

--- my-rights/src/Pages/Profile.js
+++ my-rights/src/Pages/Profile.js
@@ -7,14 +7,14 @@ const Profile = () => {
       <div className="profile">
         <div className="top">
           <h1>Profile</h1>
-          <div className="stats">
-            <div className="name">
-              <h1>Profile</h1>
+            <div className="stats">
+              <div className="name">
+                  <h1>Name</h1>
+              </div>
+              <div className="department">
+                  <h1>Department</h1>
+              </div>
             </div>
-            <div className="department">
-              <h1>Profile</h1>
-            </div>
-          </div>
         </div>
       </div>
     </>
