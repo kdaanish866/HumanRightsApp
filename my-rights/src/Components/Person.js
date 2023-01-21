a4baa1df489a5af2224f09938258ae09965c11cf
rachel <rachellin443@gmail.com>
2023-01-21 23:33:32 +0000

    added animations to person cards

--- my-rights/src/Components/Person.js
+++ my-rights/src/Components/Person.js
@@ -4,7 +4,7 @@ import '../Css/Person.css';
 const Person = ({icon, heading, desc})=> {
     return (
         <div className='person'>
-            <img src={icon} style={{ width: 80, height: 100 }} alt="person"/>
+            <img src={icon} style={{ width: 70, height: 90 }} alt="person"/>
             <span>{heading}</span>
             <span>{desc}</span>
         </div>
