df4825683990eae7b477dde73cb8a30c29384b91
sramesh <62487541+sahanarameshh@users.noreply.github.com>
2023-01-21 22:24:08 +0000

    img

--- my-rights/src/Pages/Landing.js
+++ my-rights/src/Pages/Landing.js
@@ -1,26 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../Css/Landing.css";
 
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../Css/Landing.css';
-
-const Landing=()=> {
-return(
+const Landing = () => {
+  return (
     <>
-    <div className='container'>
-        <div className='contentBox'>
-            <h1>Human Rights Project</h1>
-            <p>blah blah</p>
-            <div className='btnBox'>
-                <div className='btn'>
-                    <NavLink to="/profile" className="policeProfile" style={{ textDecoration: 'none' }}>Register as Police Officer</NavLink>
-                </div>
-                <div className='btn'>
-                    <NavLink to="/search" className="searchProfile" style={{ textDecoration: 'none' }}>Find a Police Officer</NavLink>
-                </div>
+      <div className="container">
+        <div className="contentBox">
+          <h1>Human Rights Project</h1>
+          <p>blah blah</p>
+          <div className="btnBox">
+            <div className="btn">
+              <NavLink
+                to="/profile"
+                className="policeProfile"
+                style={{ textDecoration: "none" }}
+              >
+                Register as Police Officer
+              </NavLink>
             </div>
+            <div className="btn">
+              <NavLink
+                to="/search"
+                className="searchProfile"
+                style={{ textDecoration: "none" }}
+              >
+                Find a Police Officer
+              </NavLink>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-)
-}
+  );
+};
 export default Landing;
