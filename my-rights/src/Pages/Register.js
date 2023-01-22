2431de7b6ae43e017d7f19a13d93fafb86e85803
rachel <rachellin443@gmail.com>
2023-01-22 05:19:50 +0000

    register files modified

--- my-rights/src/Pages/Register.js
+++ my-rights/src/Pages/Register.js
@@ -5,30 +5,33 @@ function Register(){
 
     const[name, setName] = useState('');
     const[badge, setBadge] = useState('');
-    const onChange = (e) => {
-        const value = e.target;
+    // const onChange = (e) => {
+    //     const value = e.target;
 
-        setName(value);
-    }
+    //     setName(value);
+    // }
 
     const showData = () => {
         console.log('Name: ', name)
         console.log('Badge: ', badge)
     }
   return (
-    <div className = "register container">
+    <div className="register container">
             <form className='register-form form-inner'>
                 <h2>Register</h2>
                 {/* ERROR! */}
                 <div className='form-group'>
                     <label htmlFor='name'>Name:</label>
-                    <input onChange={onChange} type="text" name="name" id="name" />
+                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" />
                 </div>
                 <div className='form-group'>
                     <label htmlFor='badge'>Badge Number:</label>
-                    <input onChange={onChange} type="text" name="badge" id="badge" />
+                    <input value={badge} onChange={(e) => setBadge(e.target.value)} type="text" name="badge" id="badge" />
                 </div>
                 <input onClick={showData} className="search-button"type="submit" value="Register"/>
+                <ul>
+                    <a href='#'>Already have an account? Click here.</a>
+                </ul>
             </form>
     </div>
 
