9057f6249e8afc44f74293c214aa92f93d1773e0
sramesh <62487541+sahanarameshh@users.noreply.github.com>
2023-01-22 00:30:43 +0000

    s

--- my-rights/src/Pages/Landing.js
+++ my-rights/src/Pages/Landing.js
@@ -1,36 +1,48 @@
 // CHANGES BY RACHEL
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../Css/Landing.css';
-import Hero from '../Components/Hero.js'
-import AOS from 'aos';
-import { useEffect } from 'react';
-import 'aos/dist/aos.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../Css/Landing.css";
+import Hero from "../Components/Hero.js";
+import AOS from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
 
-const Landing=()=> {
-    useEffect(() => {
-        AOS.init({
-            duration: 400,
-            easing: "ease",
-            once: false
-        });
-      });
-return(
+const Landing = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 400,
+      easing: "ease",
+      once: false,
+    });
+  });
+  return (
     <>
-    <Hero/>
-    <div className='container'>
-        <div className='contentBox'>
-            <div className='btnBox'>
-                <div className='btn' data-aos="fade-right">
-                    <NavLink to="/profile" className="policeProfile" style={{ textDecoration: 'none' }}>Register as Police Officer</NavLink>
-                </div>
-                <div className='btn' data-aos="fade-left">
-                    <NavLink to="/search" className="searchProfile" style={{ textDecoration: 'none' }}>Find a Police Officer</NavLink>
-                </div>
+      <Hero />
+      <div className="container">
+        <div className="contentBox">
+          <div className="btnBox">
+            <div className="btn" data-aos="fade-right">
+              <NavLink
+                to="/profile"
+                className="policeProfile"
+                style={{ textDecoration: "none" }}
+              >
+                Register as Police Officer
+              </NavLink>
             </div>
+            <div className="btn" data-aos="fade-left">
+              <NavLink
+                to="/search"
+                className="searchProfile"
+                style={{ textDecoration: "none" }}
+              >
+                Find a Police Officer
+              </NavLink>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-)
-}
+  );
+};
 export default Landing;
